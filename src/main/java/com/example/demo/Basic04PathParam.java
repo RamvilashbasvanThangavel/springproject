6a39d42242742fc639607f19ba2667555c163be3
RamvilashbasvanThangavel <rvbramvilash@gmail.com>
2023-07-28 12:50:17 +0530

    new commit

--- src/main/java/com/example/demo/Basic04PathParam.java
+++ src/main/java/com/example/demo/Basic04PathParam.java
@@ -2,18 +2,14 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
-import jakarta.websocket.server.PathParam;
-
 public class Basic04PathParam {
-	@GetMapping(value="/check")
-	public String nameCheck(@PathParam("usr") String usr,@PathParam("pass") String pass) {
-		if(usr.equals("Ram")&& pass.equals("1804")) {
+	@GetMapping(value = "/check")
+	public String nameCheck(String usr, String pass) {
+		if (usr.equals("Ram") && pass.equals("1804")) {
 			return "Hai";
-		}
-		else {
+		} else {
 			return "usr and pass not valid";
 		}
 	}
-	
 
 }
